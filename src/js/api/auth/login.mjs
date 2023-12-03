06bc32bf4fa13aa8c17c360e583055b3861f119d
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2023-12-03 17:13:42 +0100

    small tweaks on html and added login functionality

---
+++ src/js/api/auth/login.mjs
@@ -0,0 +1,23 @@
+import { API_SOCIAL_URL } from "../constants.mjs";
+import * as storage from "../../storage/index.mjs";
+
+const action = "/auth/login";
+const method = "post";
+
+export async function login(profile) {
+  const loginURL = API_SOCIAL_URL + action;
+  console.log(loginURL);
+
+  const response = await fetch(loginURL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    body: JSON.stringify(profile),
+  });
+
+  const { accessToken, ...user } = await response.json();
+
+  storage.save("token", accessToken);
+  storage.save("profile", user);
+}
