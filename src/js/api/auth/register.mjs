f41fe16e87b747f5347607f859464d3500b92fb9
VildeAvloes <94361976+VildeAvloes@users.noreply.github.com>
2023-12-05 14:53:41 +0100

    redirected user at login and register at success

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -4,16 +4,25 @@ const action = "/auth/register";
 const method = "post";
 
 export async function register(profile) {
-  const registerURL = API_SOCIAL_URL + action;
+  try {
+    const registerURL = API_SOCIAL_URL + action;
+    const body = JSON.stringify(profile);
 
-  const response = await fetch(registerURL, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method,
-    body: JSON.stringify(profile),
-  });
+    const response = await fetch(registerURL, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method,
+      body,
+    });
 
-  const result = await response.json();
-  return result;
+    const registerUser = await response.json();
+    window.location.href = "/feed";
+    return registerUser;
+  } catch (error) {
+    console.log(error);
+    alert(
+      "Failed to register user, please check that the requirements are met."
+    );
+  }
 }
